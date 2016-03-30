a9983795aae410e1e7b726c99b2b767a601150d7
Rado Buransky <radoburansky@gmail.com>
2016-03-30 14:06:18 +0200

    Excercise01App tasks

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 1. Clone this repo
 2. Open `build.gradle` in [IntelliJ IDEA](https://www.jetbrains.com/idea/download/).
 3. Build the project and try to run `com.pygmalios.sparkWorkshop.RunAllApp`.
+
+## Useful links
+
+[Spark 1.4 API documentation](https://spark.apache.org/docs/1.4.1/api/scala/index.html#org.apache.spark.package)
--- src/main/scala/com/pygmalios/sparkWorkshop/excercise01/Excercise01App.scala
+++ src/main/scala/com/pygmalios/sparkWorkshop/excercise01/Excercise01App.scala
@@ -1,10 +1,11 @@
 package com.pygmalios.sparkWorkshop.excercise01
 
 import com.pygmalios.sparkWorkshop.ExcerciseResources
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  * Word count.
+  * Excercise 1: word count
   */
 object Excercise01App extends App {
   // Create Spark context
@@ -13,12 +14,31 @@ object Excercise01App extends App {
     .setMaster("local[*]")
   val sc = new SparkContext(sparkConf)
 
-  val textFile = sc.textFile(ExcerciseResources.data01.toString)
-  val counts = textFile.flatMap(line => line.split(" "))
+  //=====================================================================================
+  // Create RDD from a data source
+  //=====================================================================================
+
+  // Read data from a provided text file to RDD consisting of 10 partitions
+  val textFile: RDD[String] = sc.textFile(ExcerciseResources.data01.toString, 10)
+
+  //=====================================================================================
+  // Define transformations on the RDD
+  //=====================================================================================
+
+  // Count number of occurence for all words in the text
+  val counts: RDD[(String, Int)] = textFile
+    // Split lines into individual words
+    .flatMap(line => line.split(" "))
+    // Create key-value tuples from all words, where "word" is the key and value is 1
     .map(word => (word, 1))
+    // Reduce tuples with the same key (word) using addition
     .reduceByKey(_ + _)
 
-  counts.foreach(println)
+  // TODO task 1.1: print counts to console (hint: RDD.foreach, println)
+  // TODO task 1.2: read another text file data02 and join the results
+  // TODO task 1.3: print count sorted by number of occurenece (hint: RDD.sortBy, RDD.repartition)
+  // TODO task 1.4: save results formatted as Json { "word1": count1, "word2": count2, ... } to a text file (hint: RDD.saveAsTextFile)
 
+  // Stop Spark context
   sc.stop()
 }
